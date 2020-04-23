6410d538b5f819b17e8c8ba22694b57d437f8331
perplexedcaleb <c.howard14@students.clark.edu>
2020-04-23 16:17:29 -0700

    Code completed. I tried a true or false.

---
+++ Other code.py
@@ -0,0 +1,18 @@
+"""
+CTEC 121
+Caleb Howard
+Second part of assignment
+Going to try to do some kind of questionaire game
+"""
+
+def main():
+    print("Welcome to my game! I will ask you a couple of true or false questions. Just type true or false.")
+    print("First Question: Is the year 2020?")
+    input()
+    if("True"):
+        print("Congrats you won the game!")
+        
+    else:
+        print("Sorry. You lost.")
+
+main()
---
+++ Programming Assistant.py
@@ -0,0 +1,18 @@
+"""
+CTEC 121
+Caleb Howard
+Programming Assistant
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    print("Hello everyone!")
+    print("I hope you all have a wonderful day!")
+    print("Goodbye!")
+
+main()    
